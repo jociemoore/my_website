b69352f0c5431ff268422b78b1903bdd877d30e3
Jocie Moore <hello@jociemoore.com>
2019-01-09 18:15:46 -0800

    call handleChangeLogoSource on window resize

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ window.addEventListener('load', () => {
   const html = document.querySelector('html');
   html.classList.add('fadedIn');
 
-  const windowHeight = window.innerHeight;
   const logo = document.getElementById('logo');
-  const logoTopPosition = logo.getBoundingClientRect().top;
-  const logoHeight = logo.height;
 
   // Helper Functions
   const getCurrentMidLogoPosition = () => {
     const pageTopPosition = window.scrollY;
+    const logoTopPosition = logo.getBoundingClientRect().top;
+    const logoHeight = logo.height;
     return pageTopPosition + logoTopPosition + (logoHeight / 2);
   };
 
@@ -32,6 +31,7 @@ window.addEventListener('load', () => {
   };
 
   const handleChangeLogoSource = (hovered) => {
+    const windowHeight = window.innerHeight;
     const midLogoPosition = getCurrentMidLogoPosition();
 
     if (hovered) {
@@ -45,6 +45,7 @@ window.addEventListener('load', () => {
 
   // Event Listeners
   document.addEventListener('scroll', () => handleChangeLogoSource());
+  window.addEventListener('resize', () => handleChangeLogoSource());
   logo.addEventListener('mouseenter', () => handleChangeLogoSource(true));
   logo.addEventListener('mouseleave', () => handleChangeLogoSource());
 });
